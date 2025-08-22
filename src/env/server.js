95bc7e87d98a21b69fad3d58de4d56880be6f5c8
cleeryy <clery.arqueferradou@gmail.com>
2025-08-22 19:17:13 +0200

    [~] Replaced Github auth provider with Google

--- src/env/server.js
+++ src/env/server.js
@@ -9,8 +9,8 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "test", "production"]),
     AUTH_SECRET: z.string().min(1),
     DATABASE_URL: z.string().url(),
-    AUTH_GITHUB_ID: z.string().min(1),
-    AUTH_GITHUB_SECRET: z.string().min(1),
+    AUTH_GOOGLE_ID: z.string().min(1),
+    AUTH_GOOGLE_SECRET: z.string().min(1),
   },
   /**
    * Specify runtimeEnv manually for Next.js < 13.4.4
@@ -19,8 +19,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
-    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
   },
   /**
    * Skip validation of environment variables (useful for CI/CD)
