3d669f4d2e2572f5ee31cea3316b64e48e333240
cleeryy <clery.arqueferradou@gmail.com>
2025-08-22 20:09:56 +0200

    [f!?] fixing bad github actions workflow part6

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ import "./src/env/index.js"
 
 const nextConfig = {
   // if you want to use standalone output, uncomment the following line
+  output: "standalone",
   transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  }
 };
 
 export default nextConfig;
